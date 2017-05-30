fcb9f7332338029f54e747b9eb52a7445312223c
Mauricio Matsubara <mauricio@matsubara.co>
2017-05-30 18:55:13 -0300

    exercise desc

--- Review8/main.cpp
+++ Review8/main.cpp
@@ -6,10 +6,27 @@
 //  Copyright © 2017 Mauricio Matsubara. All rights reserved.
 //
 
+/*
+ 
+ 1. Test the initial program
+ 2. Change the program to get data from the user (cin)
+ 3. change the program to get 10 numbers from the user (hint: use a loop)
+ 4. Sort the array (ascending order)
+ 
+ */
+
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int numbers[3];
+    
+    numbers[0] = 1;
+    numbers[1] = 15;
+    numbers[2] = 3;
+    
+    std::cout << numbers[0];
+    std::cout << numbers[1];
+    std::cout << numbers[2];
+    
     return 0;
 }
